86616168fc04810c0967a0c30d4503bcb6b06533
tpalinski <tpalinski@icloud.com>
2023-05-18 22:34:18 +0200

    add family guy funny moments

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,51 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "coding-boring-ah" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('coding-boring-ah.greeting', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
 		vscode.window.showInformationMessage('Your family guy funny moments are here!');
 	});
-
 	context.subscriptions.push(disposable);
+
+	// Show webview
+	let wv = vscode.commands.registerCommand('coding-boring-ah.start', () => {
+		// create webview
+		const view = vscode.window.createWebviewPanel(
+			'funny-moments',
+			'Focus Aid', //title
+			vscode.ViewColumn.Beside,
+			{
+				enableScripts: true
+			}
+		)
+
+
+		const createInitialView = () => {
+			return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>Cat Coding</title>
+			</head>
+			<body>
+				<iframe 
+					width="400" 
+					height="300" 
+					src="https://www.youtube.com/embed/T0mxP9_5RPM?autoplay=1&mute=1" 
+					title="Family guy" 
+					frameborder="0"
+					allow="autoplay"
+					allowfullscreen>
+				</iframe>
+				
+			</body>
+			</html>`;
+		}
+
+		view.webview.html = createInitialView();
+	});
+	context.subscriptions.push(wv);
 }
 
 // This method is called when your extension is deactivated
